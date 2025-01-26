d4c885f3b79587353047d2d8558eaaa046b2a1d5
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2025-01-25 22:56:48 -0300

    criar o arquivo animation separar a lógica da criação das animações

---
+++ animations.h
@@ -0,0 +1,11 @@
+#ifndef ANIMATIONS_H
+#define ANIMATIONS_H
+
+void animacaoPurpleLineInterval(void);
+void animationAllLedsYellowPairs(void);
+void animationAllOddLedsPink(void);
+void animationAllGreenLeds(void);
+void animationAllRedLeds(void);
+void animationAllBlueLeds(void);
+
+#endif
