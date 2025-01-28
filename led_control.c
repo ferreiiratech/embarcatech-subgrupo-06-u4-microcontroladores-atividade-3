f068b0c7852417c8f7cf386e04e8c81e5c518fde
nrocha46 <nicolerocha609@gmail.com>
2025-01-28 10:49:02 -0300

    Adição da função para animação do coração

--- led_control.c
+++ led_control.c
@@ -80,6 +80,9 @@ void animationFunctions(char key) {
         case '4':
             animationSEQUENCE();
             break;
+        case '6':
+            animationHeartbeat();
+            break;
         case '7':
             animationCEPEDI();
             break;
