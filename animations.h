6538775bf969981ae9657c339af93a2641a5c1dd
nrocha46 <nicolerocha609@gmail.com>
2025-01-28 10:52:10 -0300

    correção de erro

--- animations.h
+++ animations.h
@@ -7,7 +7,7 @@ void animationAllOddLedsPink(void);
 void animationAllGreenLeds(void);
 void animationAllRedLeds(void);
 void animationAllBlueLeds(void);
-void animationHeartbeat(void)
+void animationHeartbeat(void);
 void animationCEPEDI(void);
 void animationSEQUENCE(void);
 void animacaoRelogio(void);
